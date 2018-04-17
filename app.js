cef479dccbc30d358dc6558cbfc90f20d0805a70
Arunkumar Ramachandriya (Dell) <arun@dell>
2018-04-17 17:16:40 +0100

    added geocode module with old ES5 function def

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode')
 
 const argv = yargs
     .options({
@@ -14,21 +14,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-console.log(argv.address);
+// console.log(argv.address);
 
-const encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBRrp9h2nTcfiVnSTi-ZWj5edSXtB_IwNs`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to contact google servers')
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find the address')
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
+geocode.geocodeAddress(argv.address, (result) => {
+    console.log(result);
 });
+
+// console.log(geoAdd);
---
+++ geocode/geocode.js
@@ -0,0 +1,32 @@
+const request = require('request');
+
+function geocodeAddress(address, callback) {
+    const encodedAddress = encodeURIComponent(address);
+
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBRrp9h2nTcfiVnSTi-ZWj5edSXtB_IwNs`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            console.log('Unable to contact google servers');
+            callback({});
+        } else if (body.status === 'ZERO_RESULTS') {
+            console.log('Unable to find the address')
+            callback({});
+        } else if (body.status === 'OK') {
+            console.log(`Address: ${body.results[0].formatted_address}`);
+            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+
+            callback ( {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
+        }
+    })
+};
+
+module.exports = {
+    geocodeAddress
+};
